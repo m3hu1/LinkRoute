be7d3b4b8ad0e146ec2759d0947352fa492287f0
Mehul Pathak <pathakkmehul@gmail.com>
2023-12-01 13:14:12 +0530

    prettied the t&c code

--- app/termsandconditions/page.tsx
+++ app/termsandconditions/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const terms = () => {
   return (
@@ -10,37 +10,81 @@ const terms = () => {
           </h1>
           <p className="text-white text-center">
             Welcome to LinkRoute!<br></br>
-            These terms and conditions outline the rules and regulations for the use of LinkRoute's Website, located at https://linkroute.vercel.app/.
-            By accessing this website we assume you accept these terms and conditions. Do not continue to use LinkRoute if you do not agree to take all of the terms and conditions stated on this page.
-            The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of in. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
+            These terms and conditions outline the rules and regulations for the
+            use of LinkRoute's Website, located at
+            https://linkroute.vercel.app/. By accessing this website we assume
+            you accept these terms and conditions. Do not continue to use
+            LinkRoute if you do not agree to take all of the terms and
+            conditions stated on this page. The following terminology applies to
+            these Terms and Conditions, Privacy Statement and Disclaimer Notice
+            and all Agreements: "Client", "You" and "Your" refers to you, the
+            person log on this website and compliant to the Company's terms and
+            conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers
+            to our Company. "Party", "Parties", or "Us", refers to both the
+            Client and ourselves. All terms refer to the offer, acceptance and
+            consideration of payment necessary to undertake the process of our
+            assistance to the Client in the most appropriate manner for the
+            express purpose of meeting the Client's needs in respect of
+            provision of the Company's stated services, in accordance with and
+            subject to, prevailing law of in. Any use of the above terminology
+            or other words in the singular, plural, capitalization and/or he/she
+            or they, are taken as interchangeable and therefore as referring to
+            same.
           </p>
-          </div>
-          <br></br><br></br><br></br>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
         <div>
           <h1 className="smXheading1 heading2 mb-6 text-center text-gradient_blue-purple">
             Cookies
           </h1>
-          <p className='text-white text-center'>
-            We employ the use of cookies. By accessing LinkRoute, you agreed to use cookies in agreement with the LinkRoute's Privacy Policy. Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
+          <p className="text-white text-center">
+            We employ the use of cookies. By accessing LinkRoute, you agreed to
+            use cookies in agreement with the LinkRoute's Privacy Policy. Most
+            interactive websites use cookies to let us retrieve the user's
+            details for each visit. Cookies are used by our website to enable
+            the functionality of certain areas to make it easier for people
+            visiting our website. Some of our affiliate/advertising partners may
+            also use cookies.
           </p>
         </div>
-        <br></br><br></br><br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <div>
           <h1 className="smXheading1 heading2 mb-6 text-center text-gradient_blue-purple">
             License
           </h1>
-          <p className='text-white text-center'>
-          Unless otherwise stated, LinkRoute and/or its licensors own the intellectual property rights for all material on LinkRoute. All intellectual property rights are reserved. You may access this from LinkRoute for your own personal use subjected to restrictions set in these terms and conditions.<br></br><br></br>
-
+          <p className="text-white text-center">
+            Unless otherwise stated, LinkRoute and/or its licensors own the
+            intellectual property rights for all material on LinkRoute. All
+            intellectual property rights are reserved. You may access this from
+            LinkRoute for your own personal use subjected to restrictions set in
+            these terms and conditions.<br></br>
+            <br></br>
             You must not:
             <li>Republish material from LinkRoute</li>
             <li>Sell, rent or sub-license material from LinkRoute</li>
             <li>Reproduce, duplicate or copy material from LinkRoute</li>
             <li>Redistribute content from LinkRoute</li>
             <br></br>
-            This Agreement shall begin on the date hereof.
-            Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. LinkRoute does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of LinkRoute,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, LinkRoute shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.<br></br><br></br>
-            LinkRoute reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.<br></br><br></br>
+            This Agreement shall begin on the date hereof. Parts of this website
+            offer an opportunity for users to post and exchange opinions and
+            information in certain areas of the website. LinkRoute does not
+            filter, edit, publish or review Comments prior to their presence on
+            the website. Comments do not reflect the views and opinions of
+            LinkRoute,its agents and/or affiliates. Comments reflect the views
+            and opinions of the person who post their views and opinions. To the
+            extent permitted by applicable laws, LinkRoute shall not be liable
+            for the Comments or for any liability, damages or expenses caused
+            and/or suffered as a result of any use of and/or posting of and/or
+            appearance of the Comments on this website.<br></br>
+            <br></br>
+            LinkRoute reserves the right to monitor all Comments and to remove
+            any Comments which can be considered inappropriate, offensive or
+            causes breach of these Terms and Conditions.<br></br>
+            <br></br>
           </p>
         </div>
       </section>
