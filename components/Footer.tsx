43f5bd4dd08a55f15d9739f2b602391703736e36
Mehul Pathak <pathakkmehul@gmail.com>
2024-01-01 21:44:16 +0530

    updated footer for small screens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-12 max-md:flex-col glow-effect">
-      <p>Copyright © 2023 LinkRoute | All Rights Reserved</p>
+      <p className="hidden sm:inline">
+        Copyright © 2023 LinkRoute | All Rights Reserved
+      </p>
+      <p className="sm:hidden">© 2023 LinkRoute.</p>
 
       <div className="flex gap-x-9">
-        <Link href="/termsandconditions">Terms & Conditions</Link>
-        <Link href="/privacypolicy">Privacy Policy</Link>
+        <Link href="/termsandconditions" className="hidden sm:inline">
+          Terms & Conditions
+        </Link>
+        <Link href="/privacypolicy" className="hidden sm:inline">
+          Privacy Policy
+        </Link>
       </div>
     </footer>
   );
