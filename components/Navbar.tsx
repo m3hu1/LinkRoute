ac2d6485cea3dbacf1f3378f7b62016056c02126
m3hu1 <pathakkmehul@gmail.com>
2024-02-26 14:05:39 +0530

    updated assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 bg-black-100 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
-          <div className="text-gradient_purple-blue !font-bold text-5xl glow-effect-animate">
-            LR
+          <div className="text-gradient_purple-blue !font-bold text-3xl glow-effect-animate">
+            Qaution
           </div>
         </Link>
         <ul className="flex-center gap-x-3 hidden md:flex md:gap-x-10">
